ace1520b1b889eb2d77a1113899aae79f534ffec
SAYAN GHOSH <84074421+sayanthecodingchamp@users.noreply.github.com>
2024-01-07 20:47:55 +0530

    Add files via upload

---
+++ task-management-app/src/main/java/com/springdev/taskmanagementapp/registration/token/VerificationToken.java
@@ -0,0 +1,34 @@
+package com.springdev.taskmanagementapp.registration.token;
+
+import com.springdev.taskmanagementapp.user.User;
+import com.springdev.taskmanagementapp.utility.TokenExpirationTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author Sampson Alfred
+ */
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Date expirationTime;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public VerificationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationTime = TokenExpirationTime.getExpirationTime();
+    }
+}
