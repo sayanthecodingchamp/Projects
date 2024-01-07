3eda75f76bf3883b1aaf1758d1ba47d7a89b923d
SAYAN GHOSH <84074421+sayanthecodingchamp@users.noreply.github.com>
2024-01-07 20:39:55 +0530

    Add files via upload

---
+++ task-management-app/src/main/java/com/springdev/taskmanagementapp/home/HomeController.java
@@ -0,0 +1,26 @@
+package com.springdev.taskmanagementapp.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Sampson Alfred
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String homePage(){
+        return "home";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/error")
+    public String error(){
+        return "error";
+    }
+
+}
