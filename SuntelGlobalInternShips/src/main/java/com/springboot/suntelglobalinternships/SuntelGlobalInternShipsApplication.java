d784eb561c8322372ed0b1fbcbe87a9100dbe4c1
SAYAN GHOSH <84074421+sayanthecodingchamp@users.noreply.github.com>
2024-01-29 01:07:08 +0530

    Add files via upload

---
+++ SuntelGlobalInternShips/src/main/java/com/springboot/suntelglobalinternships/SuntelGlobalInternShipsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.suntelglobalinternships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuntelGlobalInternShipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuntelGlobalInternShipsApplication.class, args);
+    }
+
+}
